0b8df341c87b868f8aea6bff876705cdcd8465cc
Reshav Thakur <Reshavthkr@gmail.com>
2021-05-10 15:46:42 +0530

    chaged heading alignment

--- nutriana/src/components/Hero/Hero.js
+++ nutriana/src/components/Hero/Hero.js
@@ -6,7 +6,7 @@ function Hero() {
     return (
         <div className = "Hero" >
             <div className="heading">
-            <p className="pheading">Tell me what you eat,<br/> and I will tell you What you gain.</p>
+            <p className="pheading">Tell me what you eat,<br/>and I will tell you What you gain.</p>
             </div>
             <img src={undraw_barbecue_3x93} alt ="" className ="Image" height="400px"/>
         </div>
